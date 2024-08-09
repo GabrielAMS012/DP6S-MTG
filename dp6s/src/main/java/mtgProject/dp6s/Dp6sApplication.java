5dd001eaf3f2fc8e5d7557c95aafcc5358c00138
gabrielAMS012 <gabriel.marassi@gmail.com>
2024-08-08 21:35:12 -0300

    first commit

---
+++ dp6s/src/main/java/mtgProject/dp6s/Dp6sApplication.java
@@ -0,0 +1,13 @@
+package mtgProject.dp6s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dp6sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dp6sApplication.class, args);
+	}
+
+}
---
+++ dp6s/src/test/java/mtgProject/dp6s/Dp6sApplicationTests.java
@@ -0,0 +1,13 @@
+package mtgProject.dp6s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dp6sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
